7e0f07d282c937a5e71dc3cffbc248fe4759ece8
Arsenii Semenov <2014.semenow.senya@gmail.com>
2021-12-01 18:06:31 +0200

    Backup

--- addCar.php
+++ addCar.php
@@ -1,3 +1,25 @@
+<div class="container">
+    <form action="./addCar.php" method="POST">
+        <label>Manufacturer:
+            <input type="text" name="manufacturer">
+        </label>
+        <label>Model:
+            <input type="text" name="model">
+        </label>
+        <label>Price:
+            <input type="text" name="price">
+        </label>
+        <input type="submit" value="Add" name="sbtn">
+    </form>
+</div>
+
 <?php
-    echo "<div class='container'>test</div>"
+    if(isset($_POST['sbtn']) && isset($_POST['manufacturer']) && isset($_POST['model']) && isset($_POST['price'])) {
+        include_once('./functions.php');
+        if(addCar($_POST['manufacturer'], $_POST['model'], $_POST['price'])) {
+            echo "<div class='status'>Successfully</div>";
+        } else {
+            echo "<div class='status'>Error</div>";
+        }
+    }
 ?>
--- functions.php
+++ functions.php
@@ -10,8 +10,11 @@
             while(!feof($fd)) {
                 $carStr = fgets($fd);
                 $car = mb_split('[|]', $carStr);
-                $cars[] = ['manufacturer' => $car[0], 'model' => $car[1], 'price' => $car[2]];
+                if(count($car) > 2) {
+                    $cars[] = ['manufacturer' => $car[0], 'model' => $car[1], 'price' => $car[2]];
+                }
             }
+            fclose($fd);
         }
         return $cars;
     }
@@ -29,7 +32,46 @@
                     $counter++;
                 }
             }
+            fclose($fd);
         }
         return $counter;
     }
+
+    function addCar($manufacturer, $model, $price) {
+        $cars = getCars();
+        var_dump(array_filter($cars, function($item) {
+            global $model;
+            var_dump($model);
+            return $item['model'] == $model;
+        }));
+
+        // error_reporting(0);
+        // global $filename;
+        // $fd = fopen($filename, 'a');
+        // if($fd) {
+        //     if(fputs($fd, "$manufacturer|$model|$price\n")) {
+        //         return true;
+        //     }
+        //     fclose($fd);
+        // }
+        // return false;
+    }
+    function addCar2(&$cars, $model, $price, $year)
+    {
+        $filteredArr = array_filter($cars, function($item){
+            global $model;
+            var_dump($model);
+            return $item["model"]==$model;
+        });
+        var_dump($filteredArr);
+        // if(count($filteredArr)==0)
+        // {
+        //     $cars[] = ["model"=>$model, "price"=>$price, "year"=>$year];
+        // }
+        // else
+        // echo "<div>Данная машина уже есть в массиве!</div>";
+    }
+
+    $cars = getCars();
+    addCar2($cars, 'BMW', 'M3', 66000);
 ?>
